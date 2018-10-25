691a9ae86aa0b957c9ebdd6a3cb549705a09b514
Alexis Rieucau <alexis.rieucau@ynov.com>
2018-10-25 18:32:02 +0200

    first commit

--- nuisibles/Citadin.cs
+++ nuisibles/Citadin.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-class Citadin
+class Citadin : ITypeEcosysteme
 {
     public void Peupler(List<Nuisible> nuisibles)
     {
--- nuisibles/Rat.cs
+++ nuisibles/Rat.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-class Rat
+class Rat : Nuisible
 {
     public Rat()
     {
